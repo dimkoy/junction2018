c20c7da48cf1b2afd921ccc9525803475f21e895
dimkoy <dimkoy01@gmail.com>
2018-11-25 14:07:25 +0200

    upload final project

---
+++ ChrgMap/ChrgMap/LocationSearchTable.swift
@@ -0,0 +1,9 @@
+//
+//  LocationSearchTable.swift
+//  ChrgMap
+//
+//  Created by Дмитрий Червяков on 25/11/2018.
+//  Copyright © 2018 Дмитрий Червяков. All rights reserved.
+//
+
+import Foundation
---
+++ ChrgMap/ChrgMap/networkManager.swift
@@ -0,0 +1,9 @@
+//
+//  networkManager.swift
+//  ChrgMap
+//
+//  Created by Дмитрий Червяков on 25/11/2018.
+//  Copyright © 2018 Дмитрий Червяков. All rights reserved.
+//
+
+import Foundation
